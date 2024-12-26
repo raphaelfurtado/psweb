cf6d4f2d1f21a33250ffdf7250604924f74c39f1
Raphael Furtado <fraphaelfurtado@gmail.com>
2024-12-26 20:11:33 -0300

    Ajuste: Bem vindo(a)

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,6 +1,6 @@
 <?php echo $this->include('header', array('titulo' => $titulo)); ?>
 
-<h2>Bem-vindo, <?php echo $nome; ?></h2>
+<h2>Bem-vindo(a), <?php echo $nome; ?></h2>
 
 <p>Você é um(a) <?php echo ucfirst($role); ?>.</p>
 
