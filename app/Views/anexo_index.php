e3281a78df42ef1a169a8c9920f5c49af71038ea
vdev91 <victordeveloper91@gmail.com>
2025-01-08 16:38:34 -0300

    adaptação do cadasatro de arquivo para o novo layout

--- app/Views/anexo_index.php
+++ app/Views/anexo_index.php
@@ -1,8 +1,6 @@
-<?php echo $this->include('template/header', array('titulo' => $titulo)); ?>
+<?php echo $this->include('header', array('titulo' => $titulo)); ?>
 
-<?php echo $this->include('template/topbar'); ?>
-
-<?php echo $this->include('template/sidebar', array('role' => $role)); ?>
+<!-- < ?php echo $this->include('header', array('titulo' => $titulo)); ?> -->
 
 <?php if (session()->getFlashdata('msg_success')): ?>
     <div class="alert alert-success" role="alert" id="flash-message">
@@ -15,31 +13,6 @@
         <strong>PSWEB informa: </strong><?php echo session()->getFlashdata('msg_error'); ?>.
     </div>
 <?php endif; ?>
-
-<script>
-    function fadeOut(element, duration) {
-        let opacity = 1;  // Começar com opacidade total
-        const interval = 50; // Intervalo para diminuir a opacidade
-        const gap = interval / duration;  // O valor que vai ser subtraído da opacidade
-        // Função de animação
-        const fade = setInterval(function () {
-            opacity -= gap;
-            if (opacity <= 0) {
-                clearInterval(fade); // Parar a animação quando a opacidade atingir 0
-                element.style.display = 'none'; // Esconder o elemento
-            } else {
-                element.style.opacity = opacity; // Atualiza a opacidade do elemento
-            }
-        }, interval);
-    }
-    // Aplicar o fadeOut nas mensagens 
-    setTimeout(function () {
-        const successMessage = document.getElementById('flash-message');
-        if (successMessage) {
-            fadeOut(successMessage, 1000); // 1 segundo de fade
-        }
-    }, 2000); // Atraso de 3 segundos antes de iniciar o fade
-</script>
 
 <div class="row">
     <div class="col-lg-12 grid-margin stretch-card">
@@ -56,17 +29,17 @@
                 <br />
                 <h4 class="card-title">Documentos Cadastrados</h4>
                 <div class="table-responsive">
-                    <table id="example" class="table table-striped nowrap" style="width:100%">
+                    <table id="example" class="table table-striped nowrap table-hover" style="width:100%">
                         <thead>
                             <tr>
-                                <th>Morador</th>
-                                <th>Assunto</th>
-                                <th>Formato</th>
-                                <th>Tipo</th>
-                                <th>Quadra</th>
-                                <th>Nr da Casa</th>
-                                <th>Data Inserção</th>
-                                <th>Ações</th>
+                                <th class="desktop tablet mobile">Morador</th>
+                                <th class="desktop tablet mobile">Assunto</th>
+                                <th class="desktop">Formato</th>
+                                <th class="desktop">Tipo</th>
+                                <th class="desktop tablet mobile">Quadra</th>
+                                <th class="desktop tablet mobile">Nr da Casa</th>
+                                <th class="desktop">Data Inserção</th>
+                                <th class="desktop">Ações</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -78,12 +51,12 @@
                                     <td><?php echo $anexo['tipo_anexo']; ?></td>
                                     <td><?php echo $anexo['quadra']; ?></td>
                                     <td><?php echo $anexo['numero']; ?></td>
-                                    <td><?php echo $anexo['created_at']; ?></td>
+                                    <td class="desktop"><?php echo $anexo['created_at']; ?></td>
                                     <td>
                                         <!-- Botão para download -->
                                         <button type="button" class="btn btn-primary btn-rounded btn-icon"
                                             onclick="window.location.href='<?php echo base_url('anexo/download/' . $anexo['stored_name']); ?>'">
-                                            <i class="mdi mdi-home"></i>
+                                            <i class="mdi mdi-download"></i>
                                         </button>
 
                                         <?php if ($role === 'admin'): ?>
