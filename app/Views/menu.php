aae3bec4eb6b0234d1bf4c9a6d13298a38e1397a
Raphael Furtado <fraphaelfurtado@gmail.com>
2025-01-12 12:37:01 -0300

    Ajuste início

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -11,7 +11,7 @@ function renderMenu($role)
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo base_url('/'); ?>">
                     <i class="mdi mdi-home menu-icon"></i>
-                    <span class="menu-title">Home</span>
+                    <span class="menu-title">Início</span>
                 </a>
             </li>
             <?php if ($role === 'admin'): ?>
